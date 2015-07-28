3052aac5289af77e654f8af80e6e4d7ad4b2c905
dvrico <rico774@gmail.com>
2015-07-28 11:16:00 -0400

    Move static files to views folder and get them running

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+app.use(express.static('views'))
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.send("Hello World!")
 })
 
 var server = app.listen(3000, function() {
