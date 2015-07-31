933d29f3273d3cf7ba77a2834d2624010620226b
dvrico <rico774@gmail.com>
2015-07-31 15:51:11 -0400

    FINALLY GOT SCRIPT.JS WORKING :D (now need to apply it to app.js)

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-//Obtain an authentication token that can be used to make API requests by POSTing to
-// /api/tokens/xapp_token. Using curl:
-//curl -v -X POST "https://api.artsy.net/api/tokens/xapp_token?client_id=00665d46bb4f56d42b98&client_secret=86d483720aa6dedc9c86d1129a995749"
-
+var http = require('http')
 var request = require('superagent');
-var degasScript;
-//var elArtistInfo = document.getElementById('artist)info')
 
-var clientID = '00665d46bb4f56d42b98',
-    clientSecret = '86d483720aa6dedc9c86d1129a995749',
-    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
-    xappToken;
+//SET UP REQUEST FOR ARTSY API
+//----------------------------------
+var clientID = '00665d46bb4f56d42b98'
+var clientSecret = '86d483720aa6dedc9c86d1129a995749'
+var apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
+var xappToken;
+var degas;
 
+// SEND REQUEST
+//----------------------
 request
   .post(apiUrl)
   .send({ client_id: clientID, client_secret: clientSecret })
@@ -19,10 +19,24 @@ request
       return console.log('error')
     }
     xappToken = res.body.token;
-    getEdgar();
+    console.log(res.body)
+
+    getDegas();
   });
 
-function getEdgar(url) {
+//CREATE SERVER
+//---------------------
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write(xappToken)
+  res.write(degas)
+  res.end()
+}).listen(8000, '127.0.0.1')
+
+console.log('Server running at http://127.0.0.1:8000')
+
+
+function getDegas() {
   request
     .get('https://api.artsy.net/api/artists/edgar-degas')
     .set('X-Xapp-Token', xappToken)
@@ -31,7 +45,6 @@ function getEdgar(url) {
         return console.log('error')
       }
       console.log(res.body)
-      degasScript = res.body.name + ' was born in ' + res.body.hometown + ', ' + res.body.birthday
-      //elArtistInfo.innerHTML(degasScript)
+      degas = res.body.name + '\n' + 'Birthday: ' + res.body.birthday + '\n' + 'Hometown: ' + res.body.hometown + '\n' + 'Nationality: ' + res.body.nationality;
     })
 }
