1edbc03a1cff5b6675e960549722ed5ea21b7964
dvrico <rico774@gmail.com>
2015-08-06 16:54:42 -0400

    Add api request middleware and get token

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var express = require('express')
 var http = require('http')
 var path = require('path')
+var request = require('superagent')
 var app = express()
 
 // CONFIGURE SETTINGS
@@ -13,13 +14,34 @@ app.set('view engine', 'jade')
 
 // DEFINE MIDDLEWARE
 
+/*
+ * Request Token from Artsy API
+ */
+var clientID = '00665d46bb4f56d42b98'
+var clientSecret = '86d483720aa6dedc9c86d1129a995749'
+var apiUrl = 'https://api.artsy.net/api/tokens/xapp_token'
+var xappToken
+
+request
+  .post(apiUrl)
+  .send({ client_id: clientID, client_secret: clientSecret })
+  .end(function(err, res) {
+    if (err) {
+      return console.log('error')
+    }
+    xappToken = res.body.token;
+    console.log(res.body)
+
+    //getDegas();
+  });
+
 // DEFINE ROUTES
 app.get('/', function(req, res) {
   res.render('index', {msg: 'Welcome to the Art(sy) History Flash Card Game!'})
 })
 
 app.get('/token', function(req, res) {
-  res.render('degas')
+  res.render('degas', {msg: xappToken})
 })
 
 // START SERVER
