603b5b29ebde84aef39646b00519e341397b8a00
dvrico <rico774@gmail.com>
2015-09-13 15:04:29 -0400

    Get Mock-up page up with angular. Add necessary css libs.

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+(function() {
+    var app = angular.module('main', []);
+
+    app.controller('PanelController', ['$scope', function($scope) {
+        this.tab = 1
+        this.selectTab = function(setTab) {
+            this.tab = setTab
+        }
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab
+        }
+    }])
+
+})(); //END OF IIFE
+
 'use strict';
 
 var Artsy = require('./js/artsy.js');
