2060b0ae989df21042416e9ad6b40d3cae82b2ae
dvrico <rico774@gmail.com>
2015-09-15 12:25:04 -0400

    Refactor. Change and move promise blob of doom from controller to method in object.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@
     }]) // END OF PANEL CONTROLLER
 
     app.controller('GameController', ['$scope', function($scope) {
-        var Artsy = require('./js/artsy.js')
+
         $scope.showFirstSetOfchoices = false
         $scope.categoriesForGameSession = []
         $scope.displayCategoriesSelected = 4
@@ -33,9 +33,9 @@
 
         $scope.gameStart = function() {
             $scope.gameRound = new GameSession($scope.categoriesForGameSession)
-            getArtsyData($scope.gameRound)
+            console.log($scope.gameRound.allCategories)
+            $scope.gameRound.getArtsyData($scope.gameRound)
 
-            //correctArtwork.src = 'https://d32dm0rphc51dk.cloudfront.net/yCsq0Uq-rUQ5FuTVM--FPA/large.jpg'
             $scope.displayCategoryOne = $scope.gameRound.categoryOne
             $scope.displayCategoryTwo = $scope.gameRound.categoryTwo
             $scope.displayCategoryThree = $scope.gameRound.categoryThree
@@ -48,81 +48,7 @@
 
         }
 
-        function getArtsyData(gameRound) {
-
-            // The data-fetching promise blob of doom
-
-            // (This can become a method of the GameSession Object later on... away from everything else)
-
-            Artsy.requestToken()
-                .then(function(xappToken) {
-                    // Get Auth-token first
-                    //Artsy.xappToken = xappToken
-                    return xappToken;
-                }).then(function(xappToken) {
-                    // Choose a random category and return an array of artists.
-                    // Pause and set variables for future reference.
-
-                    var categories = $scope.categoriesForGameSession
-                    var fromRoot = 'https://api.artsy.net/api'
-                    var toPath = ['gene', 'artists']
-                    var choosenCategory = categories[randomItem($scope.categoriesForGameSession)]
-                    console.log("choosen category: ", choosenCategory)
-                    $scope.gameRound.correctCategory = choosenCategory.name
-
-                    Artsy.getArtists(fromRoot, toPath, choosenCategory.id, xappToken)
-                        .then(function(arrayOfArtists) {
-                            // Pause and set artists to multiple choice variables.
-
-                            console.log("Second then: ", arrayOfArtists)
-                            $scope.gameRound.artistOne = arrayOfArtists[0].name
-                            $scope.gameRound.artistTwo = arrayOfArtists[1].name
-                            $scope.gameRound.artistThree = arrayOfArtists[2].name
-                            $scope.gameRound.artistFour = arrayOfArtists[3].name
-                            return arrayOfArtists;
-
-                        }).then(function(arrayOfArtists) {
-                            // Choose a random artist from the group to be the
-                            // correct anwser to the current round.
-
-                            var choosenArtist = arrayOfArtists[randomItem(arrayOfArtists)]
-                            console.log("Choosen Artist: ", choosenArtist)
-                            $scope.gameRound.correctArtist = choosenArtist.name
-                            console.log('correct artist: ', $scope.gameRound.correctArtist)
-
-                            Artsy.getArtwork(choosenArtist, xappToken)
-                                .then(function(artwork) {
-                                    // Grab all available artwork from choose artist and
-                                    // select a random artwork to be the question.
-
-                                    console.log("From main.js side: ", artwork)
-                                    var correctArtwork = document.getElementById('correctArtwork')
-                                    // Set other data to variables for info in the round.
-
-                                    $scope.gameRound.correctArtworkObject = artwork[0]
-                                    $scope.gameRound.correctArtworkTitle = $scope.gameRound.correctArtworkObject.title
-                                    // Set artwork link now rather than later
-                                    correctArtwork.src = $scope.gameRound.correctArtworkObject._links.thumbnail.href
-                                    $scope.gameRound.correctArtworkLink = $scope.gameRound.correctArtworkObject._links.thumbnail.href
-
-                                    console.log("gameRound: ", $scope.gameRound.correctArtworkObject)
-                                    console.log("gameRound: ", $scope.gameRound.correctArtworkTitle)
-                                    console.log("gameRound: ", $scope.gameRound.correctArtworkLink)
-                                    //console.log($scope.gameRound.correctArtworkLink)
-                                    //$scope.updateDisplay($scope.gameRound)
-
-                                    //return $scope.gameRound.correctArtworkLink
-                                })
-                        })
-                })
-
-        }
-
-        function randomItem(array) {
-            return Math.floor(Math.random() * (array.length))
-        }
-
-        $scope.updateDisplay = function(gameRound) {
+        updateDisplay = function(gameRound) {
             $scope.displayArtistOne = $scope.gameRound.artistOne
             $scope.displayArtistTwo = $scope.gameRound.artistTwo
             $scope.displayArtistThree = $scope.gameRound.artistThree
@@ -138,28 +64,28 @@
                     console.log($scope.gameRound.correctCategory === $scope.displayCategoryOne)
                     if($scope.gameRound.correctCategory === $scope.displayCategoryOne) {
                         $scope.showFirstSetOfChoices = true
-                        $scope.updateDisplay($scope.gameRound)
+                        updateDisplay($scope.gameRound)
                     }
                     break;
                 case 2:
                     console.log($scope.gameRound.correctCategory === $scope.displayCategoryTwo)
                     if($scope.gameRound.correctCategory === $scope.displayCategoryTwo) {
                         $scope.showFirstSetOfChoices = true
-                        $scope.updateDisplay($scope.gameRound)
+                        updateDisplay($scope.gameRound)
                     }
                     break;
                 case 3:
                     console.log($scope.gameRound.correctCategory === $scope.displayCategoryThree)
                     if($scope.gameRound.correctCategory === $scope.displayCategoryThree) {
                         $scope.showFirstSetOfChoices = true
-                        $scope.updateDisplay($scope.gameRound)
+                        updateDisplay($scope.gameRound)
                     }
                     break;
                 case 4:
                     console.log($scope.gameRound.correctCategory === $scope.displayCategoryFour)
                     if($scope.gameRound.correctCategory === $scope.displayCategoryFour) {
                         $scope.showFirstSetOfChoices = true
-                        $scope.updateDisplay($scope.gameRound)
+                        updateDisplay($scope.gameRound)
                     }
                     break;
                 default:
@@ -214,8 +140,10 @@
 
     }]) // END OF GAME CONTROLLER
 
+    var Artsy = require('./js/artsy.js')
 
     function GameSession (categories) {
+        this.allCategories = categories
         this.categoryOne = categories[0].name
         this.categoryTwo = categories[1].name
         this.categoryThree = categories[2].name
@@ -233,4 +161,73 @@
         this.artistFour;
     }
 
+    GameSession.prototype.randomizer = function(array) {
+        return Math.floor(Math.random() * (array.length))
+    }
+
+    GameSession.prototype.getArtsyData = function(gameRound) {
+
+        // The data-fetching promise blob of doom
+
+        Artsy.requestToken()
+            .then(function(xappToken) {
+                // Get Auth-token first
+                return xappToken;
+            }).then(function(xappToken) {
+                // Choose a random category and return an array of artists.
+                // Pause and set variables for future reference.
+
+                var fromRoot = 'https://api.artsy.net/api'
+                var toPath = ['gene', 'artists']
+                var choosenCategory = gameRound.allCategories[gameRound.randomizer(gameRound.allCategories)]
+                console.log("choosen category: ", choosenCategory)
+                gameRound.correctCategory = choosenCategory.name
+
+                Artsy.getArtists(fromRoot, toPath, choosenCategory.id, xappToken)
+                    .then(function(arrayOfArtists) {
+                        // Pause and set artists to multiple choice variables.
+
+                        console.log("Second then: ", arrayOfArtists)
+                        gameRound.artistOne = arrayOfArtists[0].name
+                        gameRound.artistTwo = arrayOfArtists[1].name
+                        gameRound.artistThree = arrayOfArtists[2].name
+                        gameRound.artistFour = arrayOfArtists[3].name
+                        return arrayOfArtists;
+
+                    }).then(function(arrayOfArtists) {
+                        // Choose a random artist from the group to be the
+                        // correct anwser to the current round.
+
+                        var choosenArtist = arrayOfArtists[gameRound.randomizer(arrayOfArtists)]
+                        console.log("Choosen Artist: ", choosenArtist)
+                        gameRound.correctArtist = choosenArtist.name
+                        console.log('correct artist: ', gameRound.correctArtist)
+
+                        Artsy.getArtwork(choosenArtist, xappToken)
+                            .then(function(artwork) {
+                                // Grab all available artwork from choose artist and
+                                // select a random artwork to be the question.
+
+                                console.log("choosen artist artwork: ", artwork)
+                                var correctArtwork = document.getElementById('correctArtwork')
+                                // Set other data to variables for info in the round.
+
+                                gameRound.correctArtworkObject = artwork[0]
+                                gameRound.correctArtworkTitle = gameRound.correctArtworkObject.title
+                                // Set artwork link now rather than later
+                                correctArtwork.src = gameRound.correctArtworkObject._links.thumbnail.href
+                                gameRound.correctArtworkLink = gameRound.correctArtworkObject._links.thumbnail.href
+
+                                console.log("gameRound: ", gameRound.correctArtworkObject)
+                                console.log("gameRound: ", gameRound.correctArtworkTitle)
+                                console.log("gameRound: ", gameRound.correctArtworkLink)
+                                //console.log($scope.gameRound.correctArtworkLink)
+                                //updateDisplay($scope.gameRound)
+
+                                //return $scope.gameRound.correctArtworkLink
+                            })
+                    })
+            })
+
+    }
 })(); //END OF IIFE
